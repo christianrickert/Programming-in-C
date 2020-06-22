f1ecfcbbf1d34932415b2d2d13c23b6c5d060d01
Christian Rickert <mail@crickert.de>
2020-06-21 21:30:13 -0600

    Create exercise9.2.c

---
+++ exercise9.2.c
@@ -0,0 +1,36 @@
+// Program to calculate the number of days elapsed between two dates
+
+#include <stdio.h>
+
+struct date
+{
+    int month;
+    int day;
+    int year;
+};
+
+// return number of days elapsed between two dates
+int elapsedDays(struct d1, d2) {
+    int n1, n2;
+
+    n1 = 
+}
+
+int main(int argc, char const *argv[])
+{
+
+
+    struct date date1, date2;
+
+    printf("Enter first date (mm dd yyyy): ");
+    scanf("%i%i%i", &date1.month, &date1.day, &date1.year);
+    printf("");
+    printf("Enter second date (mm dd yyyy): ");
+    scanf("%i%i%i", &date2.month, &date2.day, &date2.year);
+
+
+
+    //printf("Tomorrow's date is %i/%i/%.2i.\n", date2.month, date2.day, date2.year);
+
+    return 0;
+}
