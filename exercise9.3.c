69a9a9e0e526e64d954a316ee6f6445acf42ed33
Christian Rickert <mail@crickert.de>
2020-07-02 22:41:05 -0600

    initial commit

--- exercise9.3.c
+++ exercise9.3.c
@@ -1,5 +1,7 @@
 // program that returns the elapsed time between two times
 
+#include <stdio.h>
+
 struct time
 {
     float hours;
@@ -11,42 +13,41 @@ struct time
 struct time elapsedTime(struct time time1, struct time time2)
 {
     struct time elapsedTime;
-    float elapsedSeconds = 0;
-
-    // hours
-    if ( times2.hours < times1.hours )
-        elapsedSeconds = (24 * 60 * 60) - (time1.hours - time2.hours);
-    else
-        elapsedSeconds = time2.hours - time1.hours;
-
-    // minutes
-    if ( times2.minutes < times1.minutes )
-        elapsedSeconds += (60 * 60) - (time1.minutes - time2.minutes);
-    else
-        elapsedSeconds += time2.minutes - time1.minutes;
 
     // seconds
-    if ( times2.seconds < times1.seconds )
-        elapsedSeconds += (60) - (time1.seconds - time2.seconds);
+    if ( time1.seconds > time2.seconds ) {
+        elapsedTime.seconds = 60.0 - (time1.seconds - time2.seconds);
+        elapsedTime.minutes = -1.0;  // crossing the minute border
+    } else
+        elapsedTime.seconds = time2.seconds - time1.seconds;
+
+    // minutes
+    if ( time1.minutes > time2.minutes ) {
+        elapsedTime.minutes += 60.0 - (time1.minutes - time2.minutes);
+        elapsedTime.hours = -1.0;  // crossing the hour border
+    } else
+        elapsedTime.minutes += time2.minutes - time1.minutes;
+
+    // hours
+    if ( time1.hours > time2.hours )
+        elapsedTime.hours += 24.0 - (time1.hours - time2.hours);
     else
-        elapsedSeconds += time2.seconds - time1.seconds;
+        elapsedTime.hours += time2.hours - time1.hours;
 
-    elapsedTime = {.hours = (int) ,
-                   .minutes = (int) ,
-                   .seconds = (int) };
-
+    return elapsedTime;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    struct time time1, time2, et;
-    time1 = {.hours = 3, .minutes=45, .seconds=15};
-    time2 = {.hours = 9, .minutes=44, .seconds=03};
     struct time elapsedTime(struct time time1, struct time time2);
+    struct time time1 = {.hours = 10, .minutes = 45, .seconds = 15};
+    struct time time2 = {.hours = 9, .minutes = 44, .seconds = 03};
 
-    et = elapsedTime(time1, time2);
-    printf("Number of elapsed time (rounded): %i:%i:%i.\n", (int) et.hours, et.minutes, et.seconds);
+    struct time et = elapsedTime(time1, time2);
+    printf("Time 1:  %i:%02i:%02i\n", (int) time1.hours, (int) time1.minutes, (int) time1.seconds);
+    printf("Time 2:  %i:%02i:%02i\n", (int) time2.hours, (int) time2.minutes, (int) time2.seconds);
+    printf("Elapsed: %i:%02i:%02i\n", (int) et.hours, (int) et.minutes, (int) et.seconds);
 
     return 0;
 }
