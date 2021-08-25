8e599ca52ae9d5a2f77f124ac20d3a055ee108e0
Christian Rickert <rc.email@icloud.com>
2021-08-24 20:11:27 -0600

    initial commit

---
+++ exercise16.3.c
@@ -0,0 +1,41 @@
+// Write a program to copy one file to another, replacing all
+// lowercase characters with their uppercase equivalents.
+// Using program16.3.c as template, removing input for reasons
+// of simplicity
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[])
+{
+    char inName[64], outName[64];
+    FILE *in, *out;
+    int c;
+
+    // open input and output files
+
+    if ( (in = fopen("infile.txt", "r")) == NULL )
+    {
+        printf("Can't open %s for reading.\n", "infile.txt");
+        return 1;
+    }
+    if ( (out = fopen("outfile.txt", "w")) == NULL )
+    {
+        printf("Can't open %s for writing.\n", "outfile.txt");
+        return 1;
+    }
+
+    // copy in to out
+    while ( (c = getc(in)) != EOF )
+    {
+        putc(toupper(c), out);
+    }
+
+    // close open files
+    fclose(in);
+    fclose(out);
+
+    printf("File has been converted from lowercase to uppercase.\n");
+
+    return 0;
+}
