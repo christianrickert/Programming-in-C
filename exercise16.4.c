49717026765abbcd221fdb6e2e9dc53b7f2a0a01
Christian Rickert <rc.email@icloud.com>
2021-08-24 21:03:25 -0600

    initial commit

---
+++ exercise16.4.c
@@ -0,0 +1,40 @@
+// Write a program to that merges lines from two files alternatively
+// and simply writes the results to stdout. Surplus lines should simply
+// be copied to stdout, too.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char outBuffer[81];
+    FILE *in, *and;
+    int c;
+
+    // open input and output files
+
+    if ( (in = fopen("infile.txt", "r")) == NULL )
+    {
+        printf("Can't open %s for reading.\n", "infile.txt");
+        return 1;
+    }
+    if ( (and = fopen("andfile.txt", "r")) == NULL )
+    {
+        printf("Can't open %s for reading.\n", "outfile.txt");
+        return 1;
+    }
+
+    // read both files until reaching both EOF
+    while ( !feof(in) || !feof(and) )
+    {
+        if ( fgets(outBuffer, 81, in) != NULL )
+            printf("%s\n", outBuffer);
+        if ( fgets(outBuffer, 81, and) != NULL )
+            printf("%s", outBuffer);
+    }
+
+    // close open files
+    fclose(in);
+    fclose(and);
+
+    return 0;
+}
